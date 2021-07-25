b363bd94ff084f4a641e9843a8fb56051809551a
Унжакова Екатерина <katerina.unz@gmail.com>
2021-07-26 01:34:34 +0700

    Решение task-1

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,10 @@
-const categories = document.querySelector('ul#categories');
-console.log(categories);
+const categoriesRef = document.querySelector('ul#categories');
+const itemListEl = categoriesRef.querySelectorAll('li.item');
+
+console.log(`В списке ${itemListEl.length} категории.`);
+
+itemListEl.forEach(item =>
+  console.log(`
+    - Категория: ${item.querySelector('h2').textContent}
+    - Количество элементов: ${item.querySelectorAll('li').length} `)
+);
