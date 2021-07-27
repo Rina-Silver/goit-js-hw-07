ee75b5163f054e659013af2d6b7687c891e67958
Унжакова Екатерина <katerina.unz@gmail.com>
2021-07-27 21:22:04 +0700

    Задание 3

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,30 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'White and Black Long Fur Cat',
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryRef = document.querySelector('#gallery');
+//galleryRef.style.display = 'flex';
+//galleryRef.style.listStyleType = 'none';
+galleryRef.classList.add('gallery');
+
+const createElement = ({ url, alt }) => {
+  return `<li><img src = '${url}' alt = '${alt}' width = '240'></li> `;
+};
+
+const elements = images.map(createElement).join('');
+galleryRef.insertAdjacentHTML('beforeend', elements);
+//console.log(elements);
