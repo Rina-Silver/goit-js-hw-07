4db5222d293502337c0bcf164b63a757957fc9b8
Унжакова Екатерина <katerina.unz@gmail.com>
2021-07-27 22:46:07 +0700

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,22 @@
+const decrementBtnRef = document.querySelector(
+  '#counter button[data-action="decrement"]'
+);
+const incrementBtnRef = document.querySelector(
+  '#counter button[data-action="increment"]'
+);
+const value = document.querySelector('#value');
+
+let counterValue = 0;
+
+function decrement() {
+  counterValue -= 1;
+  value.textContent = counterValue;
+}
+
+function increment() {
+  counterValue += 1;
+  value.textContent = counterValue;
+}
+
+incrementBtnRef.addEventListener('click', increment);
+decrementBtnRef.addEventListener('click', decrement);
