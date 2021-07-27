c986da5b5f8d5a36fd739ce9d04eb0f864437fbd
Унжакова Екатерина <katerina.unz@gmail.com>
2021-07-27 19:04:39 +0700

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+const createElement = ingredient => {
+  const ingredientEl = document.createElement('li');
+  ingredientEl.textContent = ingredient;
+  return ingredientEl;
+};
+
+const ingredientsList = ingredients.map(createElement);
+
+ingredientsRef.append(...ingredientsList);
