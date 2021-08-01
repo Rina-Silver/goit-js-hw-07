efcd0fbbf84ae42b50eb164b91dd2c52e5227a86
Унжакова Екатерина <katerina.unz@gmail.com>
2021-08-02 04:13:32 +0700

    Update task-08.js

---
+++ js/task-08.js
@@ -0,0 +1,4 @@
+const inputRef = document.querySelector('#controls input');
+const renderBtnRef = document.querySelector('button[data-action="render"]');
+const destroyBtnRef = document.querySelector('button[data-action="destroy"]');
+const boxRef = document.querySelector('#boxes');
