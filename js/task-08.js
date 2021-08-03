4d7057c71524105d6f01acf8c01d64802a2a741e
Унжакова Екатерина <katerina.unz@gmail.com>
2021-08-04 01:52:25 +0700

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,3 +2,15 @@ const inputRef = document.querySelector('#controls input');
 const renderBtnRef = document.querySelector('button[data-action="render"]');
 const destroyBtnRef = document.querySelector('button[data-action="destroy"]');
 const boxRef = document.querySelector('#boxes');
+
+function handleClickRender() {
+  inputRef;
+}
+
+function handleClickDestroy() {
+  boxRef.innerHTML = '';
+  inputRef.value = '';
+}
+
+enderBtnRef.addEventListener('click', handleClickRender);
+destroyBtnRef.addEventListener('click', handleClickDestroy);
