79d9d1f71e05516c4b7a7ff6a7712bae2d5f2d67
Унжакова Екатерина <katerina.unz@gmail.com>
2021-08-04 03:06:58 +0700

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -3,8 +3,18 @@ const renderBtnRef = document.querySelector('button[data-action="render"]');
 const destroyBtnRef = document.querySelector('button[data-action="destroy"]');
 const boxRef = document.querySelector('#boxes');
 
-function handleClickRender() {
-  inputRef;
+function createBoxes(amount) {
+  const newBoxes = [];
+  for (let i = 0; i < amount; i += 1) {
+    const newBox = document.createElement('div');
+    newBox.style.width = `${30 + i * 10}px`;
+    newBox.style.height = `${30 + i * 10}px`;
+    newBox.style.backgroundColor =
+      '#' + (((1 << 24) * Math.random()) | 0).toString(16);
+    newBoxes.push(newBox);
+  }
+
+  boxRef.append(...newBoxes);
 }
 
 function handleClickDestroy() {
@@ -12,5 +22,5 @@ function handleClickDestroy() {
   inputRef.value = '';
 }
 
-enderBtnRef.addEventListener('click', handleClickRender);
+renderBtnRef.addEventListener('click', () => createBoxes(inputRef.value));
 destroyBtnRef.addEventListener('click', handleClickDestroy);
